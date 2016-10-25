5278438301872a5e70b798c5ecc438de610e85a6
Arthur Loo <arthur@vanloo.nu>
2016-10-25 23:05:46 +0200

    tested

--- FloatingButton-Liquid/ViewController.swift
+++ FloatingButton-Liquid/ViewController.swift
@@ -7,19 +7,103 @@
 //
 
 import UIKit
+import LiquidFloatingActionButton
 
 class ViewController: UIViewController {
+    
+    var cells = [LiquidFloatingCell]() //datasource
+    var floatingActionButton : LiquidFloatingActionButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        createFloatingButton()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func createFloatingButton(){
+        cells.append(createButtonCell(iconName: "InfoIcon"))
+        cells.append(createButtonCell(iconName: "MapIcon"))
+        cells.append(createButtonCell(iconName: "DistanceIcon"))
+        cells.append(createButtonCell(iconName: "PhotoIcon"))
+        cells.append(createButtonCell(iconName: "ReviewIcon"))
+        cells.append(createButtonCell(iconName: "SaveIcon"))
+        
+        let floatingFrame = CGRect(x: self.view.frame.width - 56 - 16, y: self.view.frame.height - 56 - 16, width: 56, height: 56)
+        
+        let floatingButton = createButton(frame: floatingFrame, style: .up)
+        
+        self.view.addSubview(floatingButton)
+        self.floatingActionButton = floatingButton
+    }
+    
+    
+    private func createButtonCell(iconName: String) -> LiquidFloatingCell {
+        return LiquidFloatingCell(icon: UIImage(named: iconName)!)
+    }
+    
+    private func createButton(frame:CGRect, style: LiquidFloatingActionButtonAnimateStyle) -> LiquidFloatingActionButton {
+        
+        
+        let floatingActionButton = LiquidFloatingActionButton(frame: frame)
+        floatingActionButton.animateStyle = style
+        floatingActionButton.dataSource = self
+        floatingActionButton.delegate = self
+        
+        return floatingActionButton
     }
 
 
 }
+
+
+
+extension ViewController: LiquidFloatingActionButtonDataSource {
+    func numberOfCells(_ liquidFloatingActionButton: LiquidFloatingActionButton) -> Int {
+        return cells.count
+    }
+    func cellForIndex(_ index: Int) -> LiquidFloatingCell {
+        return cells[index]
+    }
+}
+
+
+
+extension ViewController: LiquidFloatingActionButtonDelegate {
+    func liquidFloatingActionButton(_ liquidFloatingActionButton: LiquidFloatingActionButton, didSelectItemAtIndex index: Int) {
+        print("You tapped button number \(index)")
+        self.floatingActionButton.close()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '9.0'
+
+target 'FloatingButton-Liquid' do
+  
+  #pod 'LiquidFloatingActionButton', '~> 1.2'
+  pod 'LiquidFloatingActionButton', :git => 'https://github.com/yoavlt/LiquidFloatingActionButton', :branch => 'master'
+  use_frameworks!
+
+  # Pods for FloatingButton-Liquid
+
+end
+
+
+
